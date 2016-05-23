d1648fdc539a1eadee879062afe4c7733f167c4e
Mustafa Onur Celik <onur.celik@logsign.net>
2016-05-23 15:54:45 +0300

    db baglantilari falan filan fasa fiso

--- index.php
+++ index.php
@@ -6,9 +6,25 @@
 		<?php include("assets/server/functions.php"); ?>
 	</head>
 	<body>
+		<?php
+			try
+			{
+				$db = new PDO("mysql:host=localhost;dbname=kanbulucu","root","root");
+			} 
+			catch(PDOException $e) 
+			{
+				echo $e->getMessage();
+			}
+			//$db->exec("SET NAMES utf-8");
+
+			// if ($db->exec("INSERT INTO donorler SET adsoyad='Mustafa Onur Çelik', eposta='onurcelik@me.com'")):
+			// 	echo $db->lastInsertId() . "basarili";
+			// else:
+			// 	echo "Hata : " . $db->errorInfo()[2];
+			// endif;
+		?>
 		<div class="container">
 			<?php include("partials/".$page."/".$subpage.".php"); ?>
-
 			<?php include("partials/common/footer.php"); ?>
 		</div>
 	</body>
--- partials/home/index.php
+++ partials/home/index.php
@@ -1,7 +1,87 @@
 <?php $activePage = "home"; ?>
+<?php if (!$_POST): ?>
 <div class="row">
-	<div class="col-sm-12">
-		kan bulucu
-
+	<div class="col-sm-6 text-center">
+        <br><br><br><br><br>
+        <div style="width:300px;" class="pull-center">
+            <form method="POST">
+                <strong>Kan Grubu :</strong>
+                <select name="kangrubu" id="" class="form-control">
+                    <?php kangruplari($db); ?>
+                </select>
+                <br>
+                <strong>Şehir :</strong>
+                <select name="sehir" id="" class="form-control">
+                    <option value="14">Bolu</option>
+                    <option value="34">İstanbul</option>
+                </select>
+                <input type="hidden" name='tip' value='kanariyorum'>
+                <hr>
+                <button class="btn btn-danger btn-block">Kan Arıyorum</button>
+            </form>
+        </div>
 	</div>
+    <div class="col-sm-6 text-center">
+        <br><br><br><br><br>
+        <div style="width:300px;" class="pull-center">
+            <form method="POST">
+                <strong>Kan Grubu :</strong>
+                <select name="kangrubu" id="" class="form-control">
+                    <option value="arhp">A RH +</option>
+                    <option value="arhn">A RH -</option>
+                </select>
+                <br>
+                <strong>Şehir :</strong>
+                <select name="sehir" id="" class="form-control">
+                    <option value="14">Bolu</option>
+                    <option value="34">İstanbul</option>
+                </select>
+                <input type="hidden" name='tip' value='kanveriyorum'>
+                <hr>
+                <button class="btn btn-primary btn-block">
+                    Kan Vermek İstiyorum
+                </button>
+            </form>
+        </div>
+    </div>
 </div>
+<?php  else: ?>
+<?php
+    switch($_POST['tip'])
+    {
+        case "kanariyorum": print_r($_POST);
+        /////////////////////////////////////////
+        // KAN ARIYORUM
+        /////////////////////////////////////////
+        ?>
+
+            <form method="POST">
+                <select name="kangrubu" id="" class="form-control" value="<?php echo $_POST['kangrubu']; ?>">
+                    <option value="arhp">A RH +</option>
+                    <option value="arhn">A RH -</option>
+                </select>
+                <br>
+                <select name="sehir" id="" class="form-control" value="<?php echo $_POST['sehir']; ?>">
+                    <option value="14">Bolu</option>
+                    <option value="34">İstanbul</option>
+                </select>
+                <input type="hidden" name='tip' value='kanariyorum'>
+                <hr>
+                <button class="btn btn-danger btn-block">Kan Arıyorum</button>
+            </form>
+        <?php     
+        break;
+
+        case "kanveriyorum": 
+        /////////////////////////////////////////
+        // KAN VERIYORUM
+        /////////////////////////////////////////
+        ?>
+            kan veriyoreeee
+
+        <?php
+        break;
+    }
+?>
+
+<?php endif; ?>
