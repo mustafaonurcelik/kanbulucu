bb5f33f58f3796efe41cd08da2d293dae630cfa9
HUSEYIN KUCUKDAL <110202020@ogrenci.ibu.edu.tr>
2016-05-25 18:14:19 +0300

    ilan detay sayfasi olusturuldu

--- partials/kan-ariyorum/ilan-detay.php
+++ partials/kan-ariyorum/ilan-detay.php
@@ -4,10 +4,13 @@
 
     $ilan = $db->query("SELECT * FROM ilanlar WHERE id='$ilanid'")->fetch();
 
-    print_r($ilan);
 
 ?>
 
+<br />
+<!-- ilan bilgileri -->
+<h2>İLAN BİLGİLERİ</h2>
+<br />
 <table class="table table-bordered">
     <tr>
         <td>Ad Soyad</td>
@@ -17,4 +20,56 @@
         <td>Kan Grubu</td>
         <td><?php slugToName($db, $ilan[kangrubu]); ?></td>
     </tr>
+    <tr>
+        <td>İl</td>
+        <td><?php echo $ilan[il]; ?></td>
+    </tr>
+        <tr>
+        <td>İlce</td>
+        <td><?php echo $ilan[ilce]; ?></td>
+    </tr>
+        <tr>
+        <td>Tarih</td>
+        <td><?php echo $ilan[tarih]; ?></td>
+    </tr>
+    </tr>
+        <tr>
+        <td>Telefon</td>
+        <td><?php echo $ilan[telefon]; ?></td>
+    </tr>
+    </tr>
+        <tr>
+        <td>Eposta</td>
+        <td><?php echo $ilan[eposta]; ?></td>
+    </tr>
+    </tr>
+        <tr>
+        <td>Not</td>
+        <td><?php echo $ilan[kullanicinotu]; ?></td>
+    </tr>
 </table>
+<!-- ilana uygun ilanlar -->
+<h2>İLANA UYAN DONÖRLER</h2>
+<br />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
