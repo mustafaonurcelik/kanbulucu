63061f0d23619abeed0f5f6f17a4d82b870cce08
Mustafa Onur Celik <onur.celik@logsign.net>
2016-05-25 15:38:33 +0300

    ilan kayit sonrasi

--- partials/kan-ariyorum/ilan-detay.php
+++ partials/kan-ariyorum/ilan-detay.php
@@ -1,1 +1,20 @@
-ilan-alindi.php
+<?php
+
+    $ilanid = $_GET[ilanid];
+
+    $ilan = $db->query("SELECT * FROM ilanlar WHERE id='$ilanid'")->fetch();
+
+    print_r($ilan);
+
+?>
+
+<table class="table table-bordered">
+    <tr>
+        <td>Ad Soyad</td>
+        <td><?php echo $ilan[adsoyad]; ?></td>
+    </tr>
+    <tr>
+        <td>Kan Grubu</td>
+        <td><?php slugToName($db, $ilan[kangrubu]); ?></td>
+    </tr>
+</table>
